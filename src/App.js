b61b367d426ec5a3eddb022be8975c3fb2811bcf
Manojdeore7 <manojddeore11>
2023-04-08 19:02:10 +0530

    Hi2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Manoj</div>;
+  return <div className="App">Ramesh</div>;
 }
 
 export default App;

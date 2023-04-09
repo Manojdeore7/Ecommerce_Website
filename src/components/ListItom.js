f325f0429930f23980377bb4bb53c9d9ce1f4c23
Manojdeore7 <manojddeore11>
2023-04-09 17:04:25 +0530

    add cart functionality

--- src/components/ListItom.js
+++ src/components/ListItom.js
@@ -3,7 +3,10 @@ function ListItom(props) {
     <div className="col-6 container">
       <h1>{props.title}</h1>
       <img src={props.image} />
-      <h3>{props.price}</h3>
+      <div style={{ display: "flex" }}>
+        <h3>{props.price}</h3>
+        <button className="btn btn-primary offset-2"> Add to Cart</button>
+      </div>
     </div>
   );
 }
